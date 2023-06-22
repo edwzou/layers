600059e27c4aee62622447262e31fde6424c5630
briantkho <briantkho@gmail.com>
2023-06-21 21:25:41 -0700

    textbox component complete

--- catnip/App.tsx
+++ catnip/App.tsx
@@ -1,14 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import GlobalStyles from './constants/GlobalStyles';
-import LikeIcon from './components/icons/LikeIcon';
+import Textbox from './components/Textbox';
+import { useState } from 'react';
 
 export default function App() {
+	// Testing return value from Textbox component
+	const [text, setText] = useState('');
+	const onInputChange = (text: string) => {
+		setText(text);
+	};
+
 	return (
 		<View style={styles.container}>
 			<Text style={[GlobalStyles.typography.header, GlobalStyles.utils.font]}>
 				Catwalk
 			</Text>
+			<Textbox onFieldChange={onInputChange} label="YEET" />
 			<StatusBar style="auto" />
 		</View>
 	);
