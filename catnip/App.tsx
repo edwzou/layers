f783ae281f3d83fb68edafbdd3b4025a694e046c
doeunkwon <bkwon38@gmail.com>
2023-07-04 23:25:41 -0700

    attempt revert App.tsx

--- catnip/App.tsx
+++ catnip/App.tsx
@@ -1,14 +1,35 @@
-import React from 'react'
+import { StatusBar } from 'expo-status-bar';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { View, StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { Stack, StackNavigation } from './utils/StackNavigation';
+
 import GlobalStyles from './constants/GlobalStyles';
-import ProfilePage from './pages/Profile/ProfilePage'
-import Profile from './pages/Profile/Profile'
+
+import LoginPage from './pages/LoginPage';
+import SignUpPage from './pages/SignUp/SignUpPage';
+import OutfitPreviewPage from './pages/OutfitPreview/OutfitPreviewPage';
 
 export default function App() {
 	return (
-		<View style={styles.container}>
-			<ProfilePage />
-		</View>
+		<NavigationContainer>
+			<GestureHandlerRootView style={{ flex: 1 }}>
+				<View style={styles.container}>
+					<Stack.Navigator screenOptions={{ headerShown: false }}>
+						<Stack.Screen name={StackNavigation.Login} component={LoginPage} />
+						<Stack.Screen
+							name={StackNavigation.OutfitPreview}
+							component={OutfitPreviewPage}
+						/>
+						<Stack.Screen
+							name={StackNavigation.SignUp}
+							component={SignUpPage}
+						/>
+					</Stack.Navigator>
+					<StatusBar style="auto" />
+				</View>
+			</GestureHandlerRootView>
+		</NavigationContainer>
 	);
 }
 
@@ -16,5 +37,5 @@ const styles = StyleSheet.create({
 	container: {
 		flex: 1,
 		backgroundColor: GlobalStyles.colorPalette.background,
-	},
+	}
 });
