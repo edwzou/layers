4946219e8db5c7345b040305c740566754fc8d20
briantkho <briantkho@gmail.com>
2023-07-12 15:44:12 -0700

    Linked pages together

--- catnip/App.tsx
+++ catnip/App.tsx
@@ -1,26 +1,27 @@
+import { StyleSheet, Platform, StatusBar, View } from 'react-native';
 import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import { StyleSheet, SafeAreaView, Platform, StatusBar } from 'react-native';
+import { useState } from 'react';
+
 import { NavigationContainer } from '@react-navigation/native';
 import { Stack } from './utils/StackNavigation';
 import { StackNavigation } from './constants/Enums';
 
-import GlobalStyles from './constants/GlobalStyles';
-
 import SignInPage from './pages/SignIn/SignInPage';
 import SignUpPage from './pages/SignUp/SignUpPage';
-import OutfitPreviewPage from './pages/OutfitPreview/OutfitPreviewPage';
 import Match from './pages/Match/Match';
 import ProfilePage from './pages/Profile/ProfilePage';
-import { useState } from 'react';
+
+import GlobalStyles from './constants/GlobalStyles';
+
 
 export default function App() {
-	const [userToken, setUserToken] = useState(null);
+	const [userToken, setUserToken] = useState("");
 	return (
 		<NavigationContainer>
 			<GestureHandlerRootView style={{ flex: 1 }}>
-				<SafeAreaView style={styles.container}>
-					<Stack.Navigator screenOptions={{ headerShown: false }}>
+				<View style={styles.container}>
+					<Stack.Navigator screenOptions={{ headerShown: false, gestureEnabled: true, gestureDirection: 'horizontal' }}>
 						{userToken === null ? (
 							<>
 								<Stack.Screen name={StackNavigation.Login} component={SignInPage} />
@@ -32,17 +33,13 @@ export default function App() {
 						) : (
 							<>
 								<Stack.Screen name={StackNavigation.Profile} component={ProfilePage} />
-								<Stack.Screen
-									name={StackNavigation.OutfitPreview}
-									component={OutfitPreviewPage}
-								/>
-								<Stack.Screen name={StackNavigation.Match} component={Match} />
+								<Stack.Screen name={StackNavigation.Match} component={Match} options={{ animation: 'slide_from_bottom', gestureDirection: 'vertical', gestureEnabled: true }} />
 							</>
 						)}
 
 					</Stack.Navigator>
 					<ExpoStatusBar style="auto" />
-				</SafeAreaView>
+				</View>
 			</GestureHandlerRootView>
 		</NavigationContainer >
 	);
