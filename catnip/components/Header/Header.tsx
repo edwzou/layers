aefd26acf31d8f68e4a5d0b2d09c6c50dd8f1e8d
briantkho <briantkho@gmail.com>
2023-07-11 18:46:48 -0700

    Header always under safe area

--- catnip/components/Header/Header.tsx
+++ catnip/components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet, Pressable } from 'react-native';
+import { View, Text, StyleSheet, Pressable, SafeAreaView } from 'react-native';
 import React from 'react';
 import GlobalStyles from '../../constants/GlobalStyles';
 import { useNavigation } from '@react-navigation/native';
@@ -16,18 +16,20 @@ type HeaderPropType = {
 const Header = ({ text, back = true, stepOver }: HeaderPropType) => {
 	const navigation = useNavigation<NativeStackNavigationProp<StackTypes>>();
 	return (
-		<View style={styles.header}>
-			{back ? <Pressable
-				onPress={() => {
-					navigation.goBack();
-				}}
-				style={{ position: 'absolute', left: 0, paddingRight: 20 }}
-			>
-				<Icon name={GlobalStyles.icons.backOutline} size={25} />
-			</Pressable> : null}
-			<Text style={GlobalStyles.typography.subtitle}>{text}</Text>
-			{stepOver ? stepOverHandler(stepOver) : null}
-		</View>
+		<SafeAreaView>
+			<View style={styles.header}>
+				{back ? <Pressable
+					onPress={() => {
+						navigation.goBack();
+					}}
+					style={{ position: 'absolute', left: 0, paddingRight: 20 }}
+				>
+					<Icon name={GlobalStyles.icons.backOutline} size={25} />
+				</Pressable> : null}
+				<Text style={GlobalStyles.typography.subtitle}>{text}</Text>
+				{stepOver ? stepOverHandler(stepOver) : null}
+			</View>
+		</SafeAreaView>
 	);
 };
 
