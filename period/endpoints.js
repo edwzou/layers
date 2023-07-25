402842c61e9123cbdae57d8f5fc6d0f9d4472b81
Joe Bu <joegbu@gmail.com>
2023-07-24 23:17:28 -0400

    tune up commit

--- period/endpoints.js
+++ period/endpoints.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-
+const { auth, requiredScopes } = require('express-oauth2-jwt-bearer');
 
 
 const postgres = require("postgres");
