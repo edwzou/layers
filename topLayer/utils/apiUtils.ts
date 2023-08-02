068fe5cdcf0dfc3da455e27df73d73d0ed84dfd0
briantkho <briantkho@gmail.com>
2023-08-02 11:12:25 -0700

    url changes on different ip addresses

--- topLayer/utils/apiUtils.ts
+++ topLayer/utils/apiUtils.ts
@@ -1,1 +1,6 @@
-export const baseUrl = 'https://localhost:1234';
+import Constants from 'expo-constants';
+const { manifest } = Constants;
+
+export const baseUrl = manifest?.debuggerHost
+	? `http://${manifest.debuggerHost.split(':').shift()}:1234`
+	: null;
