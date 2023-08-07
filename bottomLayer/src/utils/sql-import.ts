bc2acbdce7fcc9f87db6bfa670e3d3c4f64edf2c
briantkho <briantkho@gmail.com>
2023-08-07 13:55:04 -0700

    undefined values fixed

--- bottomLayer/src/utils/sql-import.ts
+++ bottomLayer/src/utils/sql-import.ts
@@ -2,6 +2,16 @@ import postgres from 'postgres';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const POSTGRES_URL = `postgres://${process.env.PGUSER}:${process.env.PGPASSWORD}@${process.env.PGHOST}/${process.env.PGDATABASE}?options=project%3D${process.env.ENDPOINT_ID}`;
+const { PG_USER, PG_PASSWORD, PG_HOST, PG_DATABASE, ENDPOINT_ID } = process.env;
+
+const checkValues = (value: string | undefined): string => {
+  if (value === undefined) {
+    return '';
+  };
+
+  return value;
+};
+
+const POSTGRES_URL = `postgres://${checkValues(PG_USER)}:${checkValues(PG_PASSWORD)}@${checkValues(PG_HOST)}/${checkValues(PG_DATABASE)}?options=project%3D${checkValues(ENDPOINT_ID)}`;
 
 export const sql = postgres(POSTGRES_URL, { ssl: 'require' });
