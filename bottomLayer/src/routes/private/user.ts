ea40bfc7c959e02a1e021a023bc08b2f828201b4
briantkho <briantkho@gmail.com>
2023-09-03 10:49:37 -0700

    reverted to ec2267d2c354c3aa47d5b890e730022f4a4276b8, most recent working state

--- bottomLayer/src/routes/private/user.ts
+++ bottomLayer/src/routes/private/user.ts
@@ -2,9 +2,6 @@ import express, { type Request, type Response } from 'express';
 import { pool } from '../../utils/sqlImport';
 import { responseCallbackDelete, responseCallbackPost, responseCallbackUpdate, responseCallbackGet } from '../../utils/responseCallback';
 import { checkAuthenticated } from '../../middleware/auth';
-import axios from 'axios';
-import { downloadURLFromS3 } from '../../s3/download-url-from-s3';
-import { uploadURIToS3 } from '../../s3/upload-uri-to-s3';
 const router = express.Router();
 
 router.get('/', (req: Request, res: Response): void => {
@@ -110,38 +107,6 @@ router.put('/', checkAuthenticated, (req: Request, res: Response): void => {
   };
 
   void updateUser();
-});
-
-// Endpoints for updating a specific user's profile picture with image URL
-router.put('/update-profile-picture', (req: Request, res: Response): void => {
-  const userId = req.user as string;
-  const { profile_picture } = req.body; // URI of frontend
-
-  if (!userId || !profile_picture) {
-    res.status(400).json({ error: 'Invalid userId or profile_picture' });
-  }
-
-  const updateProfilePicture = async (userId: string): Promise<void> => {
-    try {
-      const response = await axios.get(profile_picture, { responseType: 'arraybuffer' });
-      const imageBuffer = Buffer.from(response.data, 'binary');
-      await uploadURIToS3(imageBuffer, userId); // upload URI to S3
-    
-      const URL = await downloadURLFromS3(userId); // getting URL from S3
-      console.log(URL);
-      const updateProfilePicture = await pool.query(`
-      UPDATE backend_schema.user
-      SET profile_picture = $1
-      WHERE  uid = $2
-      `, [URL, userId]);
-      responseCallbackUpdate(null, userId, res, 'Profile Picture URL', updateProfilePicture.rowCount);
-    } catch (error) {
-      console.error(error);
-      responseCallbackUpdate(error, userId, res, 'Profile Picture URL');
-    }
-  };
-
-  void updateProfilePicture(userId);
 });
 
 module.exports = router;
