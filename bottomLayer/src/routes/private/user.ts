ae328c309c8b1f904c53035123ea2d45ea493ead
JoeDaBu <joegbu@gmail.com>
2023-09-08 15:47:08 -0700

    Commented out convertimage usage

--- bottomLayer/src/routes/private/user.ts
+++ bottomLayer/src/routes/private/user.ts
@@ -1,6 +1,11 @@
 import express, { type Request, type Response } from 'express';
 import { pool } from '../../utils/sqlImport';
-import { responseCallbackDelete, responseCallbackPost, responseCallbackUpdate, responseCallbackGet } from '../../utils/responseCallback';
+import {
+  responseCallbackDelete,
+  responseCallbackPost,
+  responseCallbackUpdate,
+  responseCallbackGet
+} from '../../utils/responseCallback';
 import { checkAuthenticated } from '../../middleware/auth';
 import { convertImage } from '../../s3/convert-image';
 const router = express.Router();
@@ -9,7 +14,10 @@ router.get('/', (req: Request, res: Response): void => {
   const userId = req.user;
   const getUser = async (): Promise<void> => {
     try {
-      const user = await pool.query('SELECT uid, first_name, last_name, email, username, profile_picture FROM backend_schema.user WHERE uid = $1', [userId]);
+      const user = await pool.query(
+        'SELECT uid, first_name, last_name, email, username, profile_picture FROM backend_schema.user WHERE uid = $1',
+        [userId]
+      );
       const result = user.rows[0];
 
       responseCallbackGet(null, result, res, 'User');
@@ -36,12 +44,24 @@ router.post('/', checkAuthenticated, (req: Request, res: Response) => {
 
   const insertUser = async (): Promise<void> => {
     try {
-      await pool.query(`
+      await pool.query(
+        `
       INSERT INTO backend_schema.user (
         first_name, last_name, email, username, password, private_option, followers, following, profile_picture
         ) VALUES ( 
           $1, $2, $3, $4, $5, $6, $7, $8, $9)`,
-      [first_name, last_name, email, username, password, private_option, followers, following, profile_picture]);
+        [
+          first_name,
+          last_name,
+          email,
+          username,
+          password,
+          private_option,
+          followers,
+          following,
+          profile_picture
+        ]
+      );
 
       responseCallbackPost(null, res, 'User');
     } catch (error) {
@@ -59,7 +79,10 @@ router.delete('/', checkAuthenticated, (req: Request, res: Response): void => {
 
   const deleteUser = async (): Promise<void> => {
     try {
-      const deleteUser = await pool.query('DELETE FROM backend_schema.user WHERE uid = $1', [userId]);
+      const deleteUser = await pool.query(
+        'DELETE FROM backend_schema.user WHERE uid = $1',
+        [userId]
+      );
       responseCallbackDelete(null, userId, res, 'User', deleteUser.rowCount);
     } catch (error) {
       responseCallbackDelete(error, userId, res, 'User');
@@ -127,8 +150,9 @@ router.put('/', checkAuthenticated, (req: Request, res: Response): void => {
   } = req.body;
   const updateUser = async (): Promise<void> => {
     try {
-      const URL = await convertImage(profile_picture, username, false);
-      const updateUser = await pool.query(`UPDATE backend_schema.user
+      // const URL = await convertImage(profile_picture, username, false);
+      const updateUser = await pool.query(
+        `UPDATE backend_schema.user
         SET first_name = $1,
             last_name = $2,
             email = $3,
@@ -139,7 +163,19 @@ router.put('/', checkAuthenticated, (req: Request, res: Response): void => {
             following = $8,
             profile_picture = $9
         WHERE uid = $10`,
-      [first_name, last_name, email, username, password, private_option, followers, following, URL, userId]);
+        [
+          first_name,
+          last_name,
+          email,
+          username,
+          password,
+          private_option,
+          followers,
+          following,
+          URL,
+          userId
+        ]
+      );
       responseCallbackUpdate(null, userId, res, 'User', updateUser.rowCount);
     } catch (error) {
       responseCallbackUpdate(error, userId, res, 'User');
