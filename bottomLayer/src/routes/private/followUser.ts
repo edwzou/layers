684a8d953d8ae2127d170c838a2e22936f7dffe1
JoeDaBu <joegbu@gmail.com>
2023-09-07 19:18:13 -0700

    Merge Checkpoint 2

--- bottomLayer/src/routes/private/followUser.ts
+++ bottomLayer/src/routes/private/followUser.ts
@@ -9,38 +9,21 @@ import { QueryManager } from "../../utils/event-emitters/queryManager";
 const router = express.Router();
 
 // Endpoint for following a user, the url parameter is the user doing the following
-<<<<<<< HEAD
 router.post("/follow/:followerId", async (req: any, res: any) => {
   // The followedId is the person getting followed. Follower is the following.
   const { followerId } = req.params;
   const { followedId } = req.body;
   console.log(followerId, followedId);
-=======
-// eslint-disable-next-line @typescript-eslint/no-misused-promises
-router.post('/follow/:userId', async (req: any, res: any) => {
-  const { userId } = req.params;
-  const { toFollowId } = req.body;
-  console.log(userId, toFollowId);
->>>>>>> main
 
   const client1 = pool.connect();
   const client2 = pool.connect();
 
-<<<<<<< HEAD
   // Perform follow user logic
   const follow = async (
     followerId: string,
     followedId: string,
   ): Promise<void> => {
-=======
-  let res1 = -1;
-  const res2 = -1;
-  const res3 = -1;
-  const errors: string[] = [];
 
-  // Perform follow user logic
-  const follow = async (uid1: string, uid2: string): Promise<void> => {
->>>>>>> main
     try {
       /*
         Following query user issues:
@@ -64,10 +47,7 @@ router.post('/follow/:userId', async (req: any, res: any) => {
         I believe these are all the cases
         */
 
-<<<<<<< HEAD
       // Adds followed to the follower's followings array
-=======
->>>>>>> main
       const following = `
           UPDATE backend_schema.user
           SET following = array_append(following, '${followedId}')
@@ -75,10 +55,7 @@ router.post('/follow/:userId', async (req: any, res: any) => {
             AND '${followedId}' <> ALL (following)
         `;
 
-<<<<<<< HEAD
       // Adds follower to the followed's followers array
-=======
->>>>>>> main
       const follower = `
         UPDATE backend_schema.user 
           SET followers = array_append(followers, '${followerId}')
@@ -86,7 +63,6 @@ router.post('/follow/:userId', async (req: any, res: any) => {
             AND '${followerId}' <> ALL (followers)
         `;
 
-<<<<<<< HEAD
       const queryManager = new QueryManager(3);
 
       const queryTrigger = once(queryManager, "proceed");
@@ -133,55 +109,12 @@ router.post('/follow/:userId', async (req: any, res: any) => {
       responseCallbackFollow(null, followerId, followedId, res);
     } catch (error) {
       responseCallbackFollow(error, followerId, followedId, res);
-=======
-      const user = pool.query(
-        'SELECT * FROM backend_schema.user WHERE uid = $1',
-        [userId]
-      );
-
-      const followingT = clientFollowTransaction(uid2, following, client1, [res1, res3], res2);
-      const followerT = clientFollowTransaction(uid1, follower, client2, [res1, res2], res3);
-      res1 = (await user).rows.length;
-      if (res1 === 0) {
-        errors.push('User1 Not Found');
-      } else {
-        errors.push('true'); // !!! @Joe, is this what you meant?
-      }
-      errors.push(await followingT);
-      errors.push(await followerT);
-      // if (errors[0]
-      // const following = await pool.query(
-      // `
-      // UPDATE backend_schema.user
-      //   SET following = array_append(following, $1)
-      //   WHERE uid = $2
-      //     AND $1 <> ALL (following)
-      //     AND EXISTS (SELECT 1 FROM backend_schema.user WHERE uid = $2)`,
-      //   [uid1, uid2]
-      // );
-      // const follower = await pool.query(
-      // `
-      // UPDATE backend_schema.user
-      //   SET followers = array_append(followers, $1)
-      //   WHERE uid = $2
-      //     AND $1 <> ALL (followers)`,
-      //   [uid2, uid1]
-      // );
-      // console.log(following)
-      responseCallbackFollow(null, uid1, uid2, res);
-    } catch (error) {
-      responseCallbackFollow(error, uid1, uid2, res);
->>>>>>> main
-    } finally {
+      } finally {
       (await client1).release;
       (await client2).release;
     }
   };
-<<<<<<< HEAD
   void follow(followerId, followedId);
-=======
-  void follow(userId, toFollowId);
->>>>>>> main
 });
 
 // Endpoint for unfollowing a user, the url parameter is the user doing the unfollowing
@@ -205,14 +138,10 @@ router.post("/unfollow/:userId", async (req: any, res: any) => {
         `
       UPDATE backend_schema.user
         SET followers = ARRAY_REMOVE(followers, $1)
-<<<<<<< HEAD
         WHERE uid = $2`,
         [uid2, uid1],
       );
 
-=======
-        WHERE uid = $2`, [uid2, uid1]);
->>>>>>> main
       // const result1 = await thread1
       // const result2 = await thread2
       // const user1 = result1.rows[0]
