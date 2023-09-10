2db807ccb325b625dc4e6d4bbbac3c422ffa793e
JoeDaBu <joegbu@gmail.com>
2023-09-09 21:36:03 -0700

    Changed AWS to follow digital OCean spaces

--- bottomLayer/src/utils/awsImport.ts
+++ bottomLayer/src/utils/awsImport.ts
@@ -6,26 +6,30 @@ dotenv.config();
 
 // Check if AWS credentials are provided in the environment
 if (
-  !process.env.AWS_BUCKET_NAME ||
-  !process.env.AWS_ACCESS_KEY ||
-  !process.env.AWS_SECRET_KEY ||
-  !process.env.AWS_BUCKET_REGION
+  !process.env.SPACES_BUCKET_NAME ||
+  !process.env.SPACES_ACCESS_KEY ||
+  !process.env.SPACES_SECRET_KEY ||
+  !process.env.SPACES_ENDPOINT ||
+  !process.env.SPACES_BUCKET_REGION
 ) {
   throw new Error('One or more AWS environment variables are not defined.');
 }
 
 // Create an S3 client instance
 const s3Client = new S3Client({
-  region: process.env.AWS_BUCKET_REGION,
+  forcePathStyle: false,
+  endpoint: process.env.SPACES_ENDPOINT,
+  // This region doesnt matter/has no impact on code
+  region: process.env.SPACES_BUCKET_REGION,
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY,
-    secretAccessKey: process.env.AWS_SECRET_KEY
+    accessKeyId: process.env.SPACES_ACCESS_KEY,
+    secretAccessKey: process.env.SPACES_SECRET_KEY
   }
 });
 
 // Function to get the bucket name
 function getBucketName(): string {
-  return process.env.AWS_BUCKET_NAME ?? '';
+  return process.env.SPACES_BUCKET_NAME ?? '';
 }
 
 // Export the S3 client instance and the getBucketName function
