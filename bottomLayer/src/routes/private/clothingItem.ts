20edfadfb24acc2134ec553a76d643b990d94e3d
JoeDaBu <joegbu@gmail.com>
2023-09-11 12:15:22 -0700

    Fixed neon database picture naming

--- bottomLayer/src/routes/private/clothingItem.ts
+++ bottomLayer/src/routes/private/clothingItem.ts
@@ -1,6 +1,10 @@
 import express, { type Request, type Response } from 'express';
 import { pool } from '../../utils/sqlImport';
-import { responseCallbackDelete, responseCallbackPost, responseCallbackUpdate } from '../../utils/responseCallback';
+import {
+  responseCallbackDelete,
+  responseCallbackPost,
+  responseCallbackUpdate
+} from '../../utils/responseCallback';
 import { checkAuthenticated } from '../../middleware/auth';
 import { convertImage } from '../../s3/convert-image';
 const router = express.Router();
@@ -12,8 +16,11 @@ router.post('/', checkAuthenticated, (req: Request, res: Response): void => {
   const insertClothingItem = async (): Promise<any> => {
     try {
       const URL = await convertImage(image, title, true);
-      await pool.query(`INSERT INTO backend_schema.clothing_item (image, category, title, brands, size, color, uid)
-      VALUES ($1, $2, $3, $4, $5, $6, $7)`, [URL, category, title, brands, size, color, uid]);
+      await pool.query(
+        `INSERT INTO backend_schema.clothing_item (image_url, category, title, brands, size, color, uid)
+      VALUES ($1, $2, $3, $4, $5, $6, $7)`,
+        [URL, category, title, brands, size, color, uid]
+      );
 
       responseCallbackPost(null, res, 'Clothing Item');
     } catch (error) {
@@ -24,25 +31,32 @@ router.post('/', checkAuthenticated, (req: Request, res: Response): void => {
 });
 
 // Endpoint for deleting a specific outfit
-router.delete('/:ciid', checkAuthenticated, (req: Request, res: Response): void => {
-  const { ciid } = req.params;
-  const deleteItem = async (ciid: string): Promise<void> => {
-    try {
-      const deleteItem = await pool.query('DELETE FROM backend_schema.clothing_item WHERE ciid = $1', [ciid]);
+router.delete(
+  '/:ciid',
+  checkAuthenticated,
+  (req: Request, res: Response): void => {
+    const { ciid } = req.params;
+    const deleteItem = async (ciid: string): Promise<void> => {
+      try {
+        const deleteItem = await pool.query(
+          'DELETE FROM backend_schema.clothing_item WHERE ciid = $1',
+          [ciid]
+        );
 
-      responseCallbackDelete(
-        null,
-        ciid,
-        res,
-        'Clothing Item',
-        deleteItem.rowCount
-      );
-    } catch (error) {
-      responseCallbackDelete(error, ciid, res, 'Clothing Item');
-    }
-  };
-  void deleteItem(ciid);
-});
+        responseCallbackDelete(
+          null,
+          ciid,
+          res,
+          'Clothing Item',
+          deleteItem.rowCount
+        );
+      } catch (error) {
+        responseCallbackDelete(error, ciid, res, 'Clothing Item');
+      }
+    };
+    void deleteItem(ciid);
+  }
+);
 
 // Endpoint for updating a specific outfit
 router.put('/:ciid', checkAuthenticated, (req: any, res: any): void => {
@@ -53,20 +67,29 @@ router.put('/:ciid', checkAuthenticated, (req: any, res: any): void => {
     // Update the outfit in the database
     try {
       const URL = await convertImage(image, title, true);
-      const updateItem = await pool.query(`
+      const updateItem = await pool.query(
+        `
       UPDATE backend_schema.clothing_item
-      SET image = $1,
+      SET image_url = $1,
           category = $2,
           title = $3,
           brands = $4,
           size = $5,
           color = $6
       WHERE ciid = $7
-      `, [URL, category, title, brands, size, color, ciid]);
+      `,
+        [URL, category, title, brands, size, color, ciid]
+      );
       // responds with successful update even when no changes are made
-      responseCallbackUpdate(null, ciid, res, "Clothing Item", updateItem.rowCount);
+      responseCallbackUpdate(
+        null,
+        ciid,
+        res,
+        'Clothing Item',
+        updateItem.rowCount
+      );
     } catch (error) {
-      responseCallbackUpdate(error, ciid, res, 'Clothing Item')
+      responseCallbackUpdate(error, ciid, res, 'Clothing Item');
     }
   };
   void updateItem(ciid);
