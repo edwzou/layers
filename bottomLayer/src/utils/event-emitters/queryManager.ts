3966147ae2cea91f42fc70f9aeb00b0f35c0d40c
JoeDaBu <joegbu@gmail.com>
2023-09-11 19:11:08 -0700

    Unfollow endpoint working

--- bottomLayer/src/utils/event-emitters/queryManager.ts
+++ bottomLayer/src/utils/event-emitters/queryManager.ts
@@ -1,5 +1,5 @@
-import { EventEmitter } from "node:events";
-import { NotFoundError } from "../../utils/Errors/NotFoundError";
+import { EventEmitter } from 'node:events';
+import { NotFoundError } from '../../utils/Errors/NotFoundError';
 
 export class QueryManager extends EventEmitter {
   queries: number;
@@ -11,71 +11,114 @@ export class QueryManager extends EventEmitter {
     this.failures = [];
   }
 
+  // All queries are complete
   allComplete() {
-    this.emit("proceed", "All Queries Were Completed", this.result);
+    this.emit('proceed', 'All Queries Were Completed', this.result);
   }
 
+  // One query completed
   complete(query: string) {
     this.queries--;
-    this.emit("Query Completed", query);
+    this.emit('Query Completed', query);
     if (this.queries === 0) {
       this.allComplete();
     }
   }
 
+  // One query failed and the failure is predicted
   failure(failure: string, query: string) {
     this.queries--;
     this.result--;
-    console.log("Query Failed. \nQuery: " + query + "\nFailure: " + failure);
+    console.log('Query Failed. \nQuery: ' + query + '\nFailure: ' + failure);
     this.failures.push(failure);
-    this.emit("Query Failure Caught", query, failure);
+    this.emit('Query Failure Caught', query, failure);
     if (this.queries === 0) {
       this.allComplete();
     }
   }
 
+  // One query faield and the failure is unknown
   utterFailure(failure: string, query: string) {
     this.queries--;
-    this.result -= -10;
+    this.result -= 10;
     console.log(
-      "Query Failed, Uncaught Failure. \nQuery: " +
+      'Query Failed, Uncaught Failure. \nQuery: ' +
         query +
-        "\nFailure: " +
-        failure,
+        '\nFailure: ' +
+        failure
     );
-    this.emit("Query Failure Uncaught", query, failure);
+    this.emit('Query Failure Uncaught', query, failure);
     if (this.queries === 0) {
       this.allComplete();
     }
   }
 
-  resolveErrorLog(followerId: string, followedId: string) {
+  // Summurize the failures for follow endpoint
+  resolveFollowErrorLog(followerId: string, followedId: string) {
     if (this.failures.length > 0) {
-      if (this.failures.length == 3) {
+      if (this.failures.length === 3) {
         throw new NotFoundError(
-          "None of the users referenced in the follow request exist\nfollower: " +
+          'None of the users referenced in the follow request exist\nfollower: ' +
             followerId +
-            "\nfollowed: " +
-            followedId,
+            '\nfollowed: ' +
+            followedId
         );
-      } else if (this.failures.length == 2) {
-        if (this.failures[0].length > 16 || this.failures[1].length > 16) {
+      } else if (this.failures.length === 2) {
+        if (this.failures[0].length > 17 || this.failures[1].length > 17) {
           throw new NotFoundError(
-            "The Follower User (the user doing the following) Referenced in the Follow Request does not Exist\nfollowerId: " +
-              followerId,
+            'The Follower User (the user doing the following) Referenced in the Follow Request does not Exist\nfollowerId: ' +
+              followerId
           );
         } else {
           throw new NotFoundError(
-            "The Follower User (the user doing the following) \nfollowerId: " +
+            'The Follower User (the user doing the following) \nfollowerId: ' +
               followerId +
-              "already follows \nfollowedId: " +
-              followedId,
+              'already follows \nfollowedId: ' +
+              followedId
           );
         }
       } else {
         throw new NotFoundError(
-          "The Followed User (the user being followed) Referenced in the Follow Request does not exist\nfollowedId: " +
-            followedId,
+          'The Followed User (the user being followed) Referenced in the Follow Request does not exist\nfollowedId: ' +
+            followedId
+        );
+      }
+    }
+  }
+
+  // Summurize the failures for unfollow endpoint
+  resolveUnFollowErrorLog(unfollowerId: string, unfollowedId: string) {
+    if (this.failures.length > 0) {
+      if (this.failures.length === 3) {
+        throw new NotFoundError(
+          'None of the users referenced in the unfollow request exist\nunfollower: ' +
+            unfollowerId +
+            '\nunfollowed: ' +
+            unfollowedId
+        );
+      } else if (this.failures.length === 2) {
+        console.log(
+          this.failures,
+          this.failures[0].length,
+          this.failures[1].length
+        );
+        if (this.failures[0].length > 19 || this.failures[1].length > 19) {
+          throw new NotFoundError(
+            'The UnFollower User (the user doing the unfollowing) Referenced in the Unfollow Request does not Exist\nunfollowerId: ' +
+              unfollowerId
+          );
+        } else {
+          throw new NotFoundError(
+            'The Unfollower User (the user doing the unfollowing) \nunfollowerId: ' +
+              unfollowerId +
+              'does not follow \nunfollowedId: ' +
+              unfollowedId
+          );
+        }
+      } else {
+        throw new NotFoundError(
+          'The Unfollowed User (the user being unfollowed) Referenced in the Unfollow Request does not exist\nunfollowedId: ' +
+            unfollowedId
         );
       }
     }
