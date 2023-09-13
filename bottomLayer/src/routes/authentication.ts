f6e962acb5de0579f9dd21b6fac7b8f4b90e6531
JoeDaBu <joegbu@gmail.com>
2023-09-13 13:12:19 -0700

    Attempt to fix authentication typescript errors. Note currently Update and Create User are failing due to multer
    Sign up typescript errors

--- bottomLayer/src/routes/authentication.ts
+++ bottomLayer/src/routes/authentication.ts
@@ -1,8 +1,9 @@
 import express from 'express';
-import { type Request, type Response } from 'express';
+import { type Request, type Response, type NextFunction } from 'express';
 import passport from 'passport';
 import { pool } from '../utils/sqlImport';
 import { hash } from 'bcrypt';
+
 const router = express.Router();
 
 router.post(
@@ -13,10 +14,9 @@ router.post(
   })
 );
 
-// eslint-disable-next-line @typescript-eslint/no-misused-promises
 router.post(
   '/signup',
-  async (req: Request, res: Response, next: any) => {
+  (req: Request, res: Response) => {
     const {
       first_name,
       last_name,
@@ -28,31 +28,36 @@ router.post(
       following,
       profile_picture
     } = req.body;
-    const hashedPass = await hash(password, 10);
-    const user = await pool.query(
-      `
-  INSERT INTO backend_schema.user (
-    first_name, last_name, email, username, password, private_option, followers, following, pp_url
-    ) VALUES (
-      $1, $2, $3, $4, $5, $6, $7, $8, $9)`,
-      [
-        first_name,
-        last_name,
-        email,
-        username,
-        hashedPass,
-        private_option,
-        followers,
-        following,
-        profile_picture
-      ]
-    );
 
-    if (user.rowCount > 0) {
-      next();
-    } else {
-      res.status(500).send('User not created');
-    }
+    const signup = async (): Promise<void> => {
+      try {
+        // Can optimize the following awaits to call run them at the same time
+        const hashedPass = await hash(password, 10);
+        const URL = await convertImage(profile_picture, username, false);
+        await pool.query(
+          `
+        INSERT INTO backend_schema.user (
+          first_name, last_name, email, username, password, private_option, followers, following, pp_url
+          ) VALUES (
+            $1, $2, $3, $4, $5, $6, $7, $8, $9)`,
+          [
+            first_name,
+            last_name,
+            email.toLowerCase(),
+            username,
+            hashedPass,
+            private_option,
+            followers,
+            following,
+            URL
+          ]
+        );
+        responseCallbackSignUp(null, res);
+      } catch (error) {
+        responseCallbackSignUp(error, res);
+      }
+    };
+    void signup();
   },
   passport.authenticate('login', {
     failureMessage: true,
@@ -60,13 +65,13 @@ router.post(
   })
 );
 
-router.get('/logout', (req: Request, res: Response, next: any) => {
-  req.logOut((err) => {
-    // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
-    if (err) {
-      return next(err);
+router.get('/logout', (req: Request, res: Response, next: NextFunction) => {
+  req.logOut((err: Error) => {
+    if (err !== null && err !== undefined) {
+      next(err);
+    } else {
+      res.send('Logged Out');
     }
-    res.send('Logged Out');
   });
 });
 
