e8c02c9ca0cfd8ba7ddfab81194b223d1c26803e
briantkho <briantkho@gmail.com>
2023-09-14 16:40:29 -0700

    increase multer file size

--- bottomLayer/src/utils/multer.ts
+++ bottomLayer/src/utils/multer.ts
@@ -1,4 +1,5 @@
 import multer from 'multer';
 
-const storage = multer.diskStorage({});
-export const upload = multer({ storage });
+const storage = multer.memoryStorage();
+const maxSize = 10 * 1024 * 1024; // 10MB
+export const upload = multer({ storage, limits: { fileSize: maxSize } });
