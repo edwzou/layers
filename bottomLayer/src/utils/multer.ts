6cbb1f67011ab8871c7884ef2f5446c6e394f48d
briantkho <briantkho@gmail.com>
2023-09-14 16:44:43 -0700

    increase multer field size

--- bottomLayer/src/utils/multer.ts
+++ bottomLayer/src/utils/multer.ts
@@ -1,5 +1,10 @@
 import multer from 'multer';
 
 const storage = multer.memoryStorage();
-const maxSize = 10 * 1024 * 1024; // 10MB
-export const upload = multer({ storage, limits: { fileSize: maxSize } });
+const maxFileSize = 10 * 1024 * 1024; // 10MB
+const maxFieldSize = 25 * 1024 * 1024;
+
+export const upload = multer({
+  storage,
+  limits: { fileSize: maxFileSize, fieldSize: maxFieldSize }
+});
