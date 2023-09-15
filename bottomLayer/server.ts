914e8de149e6cff2cbb40f1fd43d5e176fed2139
briantkho <briantkho@gmail.com>
2023-09-14 17:33:28 -0700

    added comments to test auth

--- bottomLayer/server.ts
+++ bottomLayer/server.ts
@@ -55,31 +55,30 @@ passport.use(
   new LocalStrategy(
     { usernameField: 'email', passwordField: 'password' },
     async (email: string, password: string, done: any) => {
-      console.log('in login');
       try {
         const result = await pool.query(
           'SELECT * FROM backend_schema.user WHERE email = $1',
           [email]
         );
-        console.log('testlogin');
 
         if (result.rowCount === 0) {
           return done(null, false, { message: 'user not found' });
         }
 
         const hashedPassword = result.rows[0].password;
+        console.log(hashedPassword, password);
         const passwordMatches: boolean = await compare(
           password,
           hashedPassword
         );
-
+        console.log('match' + passwordMatches);
         if (!passwordMatches) {
           console.log('Invalid Credentials');
           return done(null, false, { message: 'invalid credentials' });
         }
-
+        console.log('before user');
         const user = result.rows[0].uid;
-
+        console.log('after user: ' + user);
         return done(null, user);
       } catch (err) {
         return done(err);
