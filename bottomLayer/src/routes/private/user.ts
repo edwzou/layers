116f4f5d8a44cdf50fc81d804837b3db0f96063d
briantkho <briantkho@gmail.com>
2023-09-21 14:42:00 -0700

    add back specific data needed from users

--- bottomLayer/src/routes/private/user.ts
+++ bottomLayer/src/routes/private/user.ts
@@ -17,7 +17,7 @@ router.get('/', checkAuthenticated, (req: Request, res: Response): void => {
   const getUser = async (): Promise<void> => {
     try {
       const user = await pool.query(
-        'SELECT * FROM backend_schema.user WHERE uid = $1',
+        'SELECT uid, first_name, last_name, email, username, private_option, followers, following, pp_url FROM backend_schema.user WHERE uid = $1',
         [userId]
       );
       const result = user.rows[0];
