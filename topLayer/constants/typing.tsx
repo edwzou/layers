7831a8603b21fc1e93c0edca8e8fb63197862eb7
JoeDaBu <joegbu@gmail.com>
2023-11-30 16:48:07 -0800

    Undid my navigation changes, and removed unsued call stepOver

--- topLayer/constants/typing.tsx
+++ topLayer/constants/typing.tsx
@@ -1,7 +1,7 @@
 import type { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { type StackTypes } from '../utils/StackNavigation';
 
-type BasicNavigationProp = NativeStackNavigationProp<StackTypes>;
+export type BasicNavigationProp = NativeStackNavigationProp<StackTypes>;
 
 export type NavigationProp = {
 	navigation: BasicNavigationProp;
--- topLayer/pages/SignIn/SignInPage.tsx
+++ topLayer/pages/SignIn/SignInPage.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Text, StyleSheet, Pressable, Keyboard } from 'react-native';
 import { StackNavigation } from '../../constants/Enums';
-
+import { useNavigation } from '@react-navigation/native';
+import { type NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { type StackTypes } from 'utils/StackNavigation';
 import GlobalStyles from '../../constants/GlobalStyles';
 import SignIn from './SignIn';
-import { NavigationProp } from 'constants/typing';
 
-const LoginPage: React.FC<NavigationProp> = ({ navigation }) => {
+const LoginPage: React.FC = () => {
+	const navigation = useNavigation<NativeStackNavigationProp<StackTypes>>();
 	return (
 		<Pressable onPress={Keyboard.dismiss} style={styles.container}>
 			<Text style={[GlobalStyles.typography.header, GlobalStyles.utils.font]}>
