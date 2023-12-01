e670bd27c9e4819ee0678f0489f1843516c35a80
JoeDaBu <joegbu@gmail.com>
2023-11-30 16:48:22 -0800

    Removed typing.tsx

--- topLayer/constants/typing.tsx
+++
@@ -1,8 +0,0 @@
-import type { NativeStackNavigationProp } from '@react-navigation/native-stack';
-import { type StackTypes } from '../utils/StackNavigation';
-
-export type BasicNavigationProp = NativeStackNavigationProp<StackTypes>;
-
-export type NavigationProp = {
-	navigation: BasicNavigationProp;
-};
