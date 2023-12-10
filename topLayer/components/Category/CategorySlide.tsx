b6fb2843bf04d2c8858c40e35c4fa94a26b84b17
JoeDaBu <joegbu@gmail.com>
2023-12-10 14:35:12 -0800

    Fixed bug with leaving the find page not rerendering the marked user list

--- topLayer/components/Category/CategorySlide.tsx
+++ topLayer/components/Category/CategorySlide.tsx
@@ -1,9 +1,8 @@
 import { StyleSheet, View, FlatList, Text, Dimensions } from 'react-native';
 import React, { useEffect } from 'react';
 
-import Icon from 'react-native-remix-icon';
 import { ITEM_SIZE } from '../../utils/GapCalc';
-import { screenWidth, screenHeight } from '../../utils/modalMaxShow';
+import { screenHeight } from '../../utils/modalMaxShow';
 import OutfitCard from '../Card/OutfitCard';
 import ItemCell from '../Cell/ItemCell';
 
@@ -12,77 +11,95 @@ import GlobalStyles from '../../constants/GlobalStyles';
 
 import { UserClothing, UserAllItems } from '../../pages/Match';
 import { UserOutfit } from '../../pages/OutfitView';
+import Empty from './Empty';
 
 interface CategorySlidePropsType {
 	// ex) [{"clothing_items": {"bottoms": [Array], "outerwear": [Array], "shoes": [Array], "tops": [Array]}, "created_at": "2023-10-14T07:15:07.986Z", "oid": "a11bdae9-9ecb-48f4-8ac2-802809034a0f", "title": "Weekend Casual", "uid": "890e7fad-1352-4998-8f2f-ff8b74b04b86"}]
-	itemsData: UserAllItems; //! !! fix any type
+	itemsData: UserAllItems;
 	handleItemChange: (item: any) => void;
 }
 
-const CategorySlide = ({ itemsData, handleItemChange }: CategorySlidePropsType) => {
-
+const CategorySlide = ({
+	itemsData,
+	handleItemChange,
+}: CategorySlidePropsType) => {
 	const windowWidth = Dimensions.get('window').width;
 
 	// useEffect(() => {
 	// 	console.log(itemsData)
 	// }, []);
 
-	return (
-		<View style={[styles.container, { width: windowWidth - 2 * GlobalStyles.layout.xGap }]}>
-			{itemsData.data && itemsData.data.length !== 0 ?
-				(
-					itemsData.category == 'outfits' ? (
-						<FlatList
-							data={itemsData.data as UserOutfit[]}
-							renderItem={({ item }) => {
-								//console.log(item)
-								return (
-									<OutfitCard
-										title={item.title}
-										clothingItems={item.clothing_items}
-										onPress={() => { handleItemChange(item) }}
-									/>
-								)
-							}}
-							showsVerticalScrollIndicator={false}
-							contentContainerStyle={{ gap: GlobalStyles.layout.gap }}
-							ListFooterComponent={
-								<View style={{ padding: screenHeight * 0.15 }} />
-							}
-						/>
-					) : (
-						<FlatList
-							data={itemsData.data as UserClothing[]}
-							renderItem={({ item }) => {
-								//console.log(item);
-								return (
-									<View style={{ width: ITEM_SIZE(2) }}>
-										<ItemCell
-											imageUrl={item.image_url}
-											key={item.ciid}
-											onPress={() => handleItemChange(item)}
-										/>
-									</View>
-								)
-							}}
-							numColumns={2}
-							contentContainerStyle={{ gap: GlobalStyles.layout.gap }}
-							columnWrapperStyle={{ gap: GlobalStyles.layout.gap }}
-							showsVerticalScrollIndicator={false}
-							ListFooterComponent={
-								<View style={{ padding: screenHeight * 0.15 }} />
-							}
-						/>
-					)
-				) :
-				<View style={{ alignItems: 'center', flex: 1, top: GlobalStyles.layout.pageStateTopMargin, gap: 5, width: screenWidth - GlobalStyles.layout.xGap * 2 }}>
-					<Icon
-						name={GlobalStyles.icons.bubbleOutline}
-						color={GlobalStyles.colorPalette.primary[300]}
-						size={GlobalStyles.sizing.icon.large}
+	const slide = () => {
+		// console.log('items data: ', windowWidth, itemsData, itemsData.data.length);
+		if (
+			itemsData.data &&
+			(itemsData.data.length !== 0 || itemsData.category === 'outfits')
+		) {
+			// console.log('rendering list');
+			if (itemsData.category === 'outfits') {
+				return (
+					<FlatList
+						data={itemsData.data as UserOutfit[]}
+						renderItem={({ item }) => {
+							//console.log(item)
+							return (
+								<OutfitCard
+									title={item.title}
+									clothingItems={item.clothing_items}
+									onPress={() => {
+										handleItemChange(item);
+									}}
+								/>
+							);
+						}}
+						numColumns={2}
+						showsVerticalScrollIndicator={false}
+						contentContainerStyle={{ gap: GlobalStyles.layout.gap }}
+						columnWrapperStyle={{ gap: GlobalStyles.layout.gap }}
+						ListFooterComponent={
+							<View style={{ padding: screenHeight * 0.15 }} />
+						}
 					/>
-					<Text style={[GlobalStyles.typography.subtitle, { color: GlobalStyles.colorPalette.primary[300] }]}>Empty</Text>
-				</View>}
+				);
+			} else {
+				return (
+					<FlatList
+						data={itemsData.data as UserClothing[]}
+						renderItem={({ item }) => {
+							//console.log(item);
+							return (
+								<View style={{ width: ITEM_SIZE(2) }}>
+									<ItemCell
+										imageUrl={item.image_url}
+										key={item.ciid}
+										onPress={() => handleItemChange(item)}
+									/>
+								</View>
+							);
+						}}
+						numColumns={2}
+						showsVerticalScrollIndicator={false}
+						contentContainerStyle={{ gap: GlobalStyles.layout.gap }}
+						columnWrapperStyle={{ gap: GlobalStyles.layout.gap }}
+						ListFooterComponent={
+							<View style={{ padding: screenHeight * 0.15 }} />
+						}
+					/>
+				);
+			}
+		} else {
+			// console.log('not rendering list');
+			return <Empty />;
+		}
+	};
+	return (
+		<View
+			style={[
+				styles.container,
+				{ width: windowWidth - 2 * GlobalStyles.layout.xGap },
+			]}
+		>
+			{slide()}
 		</View>
 	);
 };
@@ -94,4 +111,3 @@ const styles = StyleSheet.create({
 		marginHorizontal: GlobalStyles.layout.xGap,
 	},
 });
-
