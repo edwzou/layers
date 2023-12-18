dfe36f3efe4289075cd6614ea4f764b6b7820506
Doeun Kwon <dekwooon@gmail.com>
2023-12-18 12:44:36 -0800

    enable handleViewableItemsChanged again

--- topLayer/components/Category/CategorySlides.tsx
+++ topLayer/components/Category/CategorySlides.tsx
@@ -14,63 +14,63 @@ import { UserOutfit } from '../../pages/OutfitView';
 import Empty from './Empty';
 
 interface CategorySlidesPropsType {
-	categorySlidesRef: any; /// !!! Fix any type
-	allItemsData: UserAllItems[];
-	selectedCategory: string;
-	handleItemChange: (item: any) => void;
-	handleViewableItemsChanged: ({ viewableItems }: any) => void;
+    categorySlidesRef: any; /// !!! Fix any type
+    allItemsData: UserAllItems[];
+    selectedCategory: string;
+    handleItemChange: (item: any) => void;
+    handleViewableItemsChanged: ({ viewableItems }: any) => void;
 }
 
 const CategorySlides = ({
-	categorySlidesRef,
-	allItemsData,
-	selectedCategory,
-	handleItemChange,
-	handleViewableItemsChanged,
+    categorySlidesRef,
+    allItemsData,
+    selectedCategory,
+    handleItemChange,
+    handleViewableItemsChanged,
 }: CategorySlidesPropsType) => {
-	const windowWidth = Dimensions.get('window').width;
-	// console.log('data: ', allItemsData);
+    const windowWidth = Dimensions.get('window').width;
+    // console.log('data: ', allItemsData);
 
-	return (
-		<FlatList
-			ref={categorySlidesRef}
-			data={allItemsData}
-			renderItem={({ item }) => {
-				console.log('categoryslide: ', item.data);
-				return (
-					// {/* <Text */}
-					// {/* 	style={{ */}
-					// {/* 		width: screenWidth, */}
-					// {/* 		// width: GlobalStyles.layout.gap, */}
-					// {/* 		backgroundColor: 'red', */}
-					// {/* 		// borderColor: 'black', */}
-					// {/* 		// borderWidth: 10, */}
-					// {/* 		// borderStyle: 'solid', */}
-					// {/* 	}} */}
-					// {/* > */}
-					// {/* 	Empty */}
-					// {/* </Text> */}
-					// <Empty />
-					<CategorySlide itemsData={item} handleItemChange={handleItemChange} />
-				);
-			}}
-			keyExtractor={(item) => {
-				return item.category;
-			}}
-			horizontal
-			pagingEnabled
-			snapToAlignment="center"
-			showsHorizontalScrollIndicator={false}
-			// onViewableItemsChanged={handleViewableItemsChanged}
-			viewabilityConfig={{ itemVisiblePercentThreshold: 100 }}
-			initialScrollIndex={CategoryToIndex[selectedCategory]}
-			getItemLayout={(data, index) => ({
-				length: windowWidth,
-				offset: windowWidth * index,
-				index,
-			})}
-		/>
-	);
+    return (
+        <FlatList
+            ref={categorySlidesRef}
+            data={allItemsData}
+            renderItem={({ item }) => {
+                console.log('categoryslide: ', item.data);
+                return (
+                    // {/* <Text */}
+                    // {/* 	style={{ */}
+                    // {/* 		width: screenWidth, */}
+                    // {/* 		// width: GlobalStyles.layout.gap, */}
+                    // {/* 		backgroundColor: 'red', */}
+                    // {/* 		// borderColor: 'black', */}
+                    // {/* 		// borderWidth: 10, */}
+                    // {/* 		// borderStyle: 'solid', */}
+                    // {/* 	}} */}
+                    // {/* > */}
+                    // {/* 	Empty */}
+                    // {/* </Text> */}
+                    // <Empty />
+                    <CategorySlide itemsData={item} handleItemChange={handleItemChange} />
+                );
+            }}
+            keyExtractor={(item) => {
+                return item.category;
+            }}
+            horizontal
+            pagingEnabled
+            snapToAlignment="center"
+            showsHorizontalScrollIndicator={false}
+            onViewableItemsChanged={handleViewableItemsChanged}
+            viewabilityConfig={{ itemVisiblePercentThreshold: 100 }}
+            initialScrollIndex={CategoryToIndex[selectedCategory]}
+            getItemLayout={(data, index) => ({
+                length: windowWidth,
+                offset: windowWidth * index,
+                index,
+            })}
+        />
+    );
 };
 
 export default CategorySlides;
