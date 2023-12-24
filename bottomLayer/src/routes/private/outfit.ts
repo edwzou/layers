aab253d59ce6b4a5dfa4ff5a4f7f9cb9d31d5594
JoeDaBu <joegbu@gmail.com>
2023-12-24 00:49:38 -0700

    Fixed bug with updating user and clothing item uuid arrays

--- bottomLayer/src/routes/private/outfit.ts
+++ bottomLayer/src/routes/private/outfit.ts
@@ -1,135 +1,140 @@
 import {
-  responseCallbackDelete,
-  responseCallbackPost,
-  responseCallbackUpdate
+	responseCallbackDelete,
+	responseCallbackPost,
+	responseCallbackUpdate,
 } from '../../utils/responseCallback';
 import { pool } from '../../utils/sqlImport';
 import express, { type Request, type Response } from 'express';
 import {
-  getAllOutfits,
-  getAllOutfitsCate,
-  getOutfitById,
-  getOutfitByIdCate,
-  outfitCateQuery
+	getAllOutfits,
+	getAllOutfitsCate,
+	getOutfitById,
+	getOutfitByIdCate,
+	outfitCateQuery,
 } from '../helper/outfit';
 
 const router = express.Router();
 
 // Endpoint for creating a new outfit
 router.post('/', (req: Request, res: Response): void => {
-  const uid = req.user as string;
-  const { title, clothing_items } = req.body;
+	const uid = req.user as string;
+	const { title, clothing_items } = req.body;
 
-  const insertOutfit = async (): Promise<any> => {
-    try {
-      await pool.query(
-        'INSERT INTO backend_schema.outfit (title, clothing_items, uid) VALUES ($1, $2, $3)',
-        [title, clothing_items, uid]
-      );
+	const insertOutfit = async (): Promise<any> => {
+		try {
+			await pool.query(
+				'INSERT INTO backend_schema.outfit (title, clothing_items, uid) VALUES ($1, $2, $3)',
+				[title, clothing_items, uid]
+			);
 
-      responseCallbackPost(null, res, 'Outfit');
-    } catch (error) {
-      responseCallbackPost(error, res);
-    }
-  };
+			responseCallbackPost(null, res, 'Outfit');
+		} catch (error) {
+			responseCallbackPost(error, res);
+		}
+	};
 
-  void insertOutfit();
+	void insertOutfit();
 });
 
 // Endpoint for deleting a specific outfit
 router.delete('/:outfitId', (req: Request, res: Response): void => {
-  const uid = req.user as string;
-  const { outfitId } = req.params;
-  const deleteOutfit = async (outfitId: string): Promise<void> => {
-    try {
-      const deleteOutfit = await pool.query(
-        'DELETE FROM backend_schema.outfit WHERE oid = $1 AND uid = $2',
-        [outfitId, uid]
-      );
-      responseCallbackDelete(
-        null,
-        outfitId,
-        res,
-        'Outfit',
-        deleteOutfit.rowCount
-      );
-    } catch (error) {
-      responseCallbackDelete(error, outfitId, res, 'Outfit');
-    }
-  };
+	const uid = req.user as string;
+	const { outfitId } = req.params;
+	const deleteOutfit = async (outfitId: string): Promise<void> => {
+		try {
+			const deleteOutfit = await pool.query(
+				'DELETE FROM backend_schema.outfit WHERE oid = $1 AND uid = $2',
+				[outfitId, uid]
+			);
+			responseCallbackDelete(
+				null,
+				outfitId,
+				res,
+				'Outfit',
+				deleteOutfit.rowCount
+			);
+		} catch (error) {
+			responseCallbackDelete(error, outfitId, res, 'Outfit');
+		}
+	};
 
-  void deleteOutfit(outfitId);
+	void deleteOutfit(outfitId);
 });
 
 // Endpoint for updating a specific outfit
 router.put('/:oid', (req: Request, res: Response): void => {
-  const uid = req.user as string;
-  const { oid } = req.params;
-  // Extract outfit data from the request body
-  const { title, clothing_items }: { title: string; clothing_items: string[] } = req.body;
+	const uid = req.user as string;
+	const { oid } = req.params;
+	// Extract outfit data from the request body
+	const { title, clothing_items }: { title: string; clothing_items: string[] } =
+		req.body;
 
-  const updateOutfit = async (oid: string): Promise<void> => {
-    // Initialize query parts
-    let query = 'UPDATE backend_schema.outfit SET';
+	const updateOutfit = async (oid: string): Promise<void> => {
+		// Initialize query parts
+		let query = 'UPDATE backend_schema.outfit SET';
 
-    // Append title to the query if provided
-    if (title !== undefined) {
-      query += ` title = '${title}',`;
-    }
+		// Append title to the query if provided
+		if (title !== undefined) {
+			query += ` title = '${title}',`;
+		}
 
-    // Append clothing_items to the query if provided
-    if (clothing_items !== undefined) {
-      query += `clothing_items = ARRAY[${clothing_items.join(', ')}]::UUID[],`;
-    }
+		// Append clothing_items to the query if provided
+		if (clothing_items !== undefined) {
+			const formattedUuidArray = clothing_items.map(
+				(uuid) => `'${uuid}'::UUID`
+			);
+			query += ` clothing_items = ARRAY[${formattedUuidArray.join(', ')}],`;
+		}
 
-    // Finalize the query
-    if (query === 'UPDATE backend_schema.user SET') {
-      throw new Error('No Fields To Update');
-    }
-    query = query.slice(0, -1);
-    query += ` WHERE oid = '${oid}' AND uid = '${uid}'`;
+		try {
+			// Finalize the query
+			if (query === 'UPDATE backend_schema.outfit SET') {
+				throw new Error('No Fields To Update');
+			}
+			query = query.slice(0, -1);
+			query += ` WHERE oid = '${oid}' AND uid = '${uid}'`;
 
-    // Update the outfit in the database
-    try {
-      const updateOutfit = await pool.query(query);
+			console.log('update fit: ', query);
+			// Update the outfit in the database
+			const updateOutfit = await pool.query(query);
 
-      // responds with successful update even when no changes are made
-      responseCallbackUpdate(null, oid, res, 'Outfit', updateOutfit.rowCount);
-    } catch (error) {
-      responseCallbackUpdate(error, oid, res, 'Outfit');
-    }
-  };
+			// responds with successful update even when no changes are made
+			responseCallbackUpdate(null, oid, res, 'Outfit', updateOutfit.rowCount);
+		} catch (error) {
+			responseCallbackUpdate(error, oid, res, 'Outfit');
+		}
+	};
 
-  void updateOutfit(oid);
+	void updateOutfit(oid);
 });
 
 // Endpoint for retrieving the logged in users outfit
 router.get('/:outfitId', (req: Request, res: Response): void => {
-  const uid = req.user as string;
-  const { outfitId } = req.params;
-  const { parse } = req.query;
+	const uid = req.user as string;
+	const { outfitId } = req.params;
+	const { parse } = req.query;
 
-  if (parse === 'categories') {
-    const queryAdvanced = outfitCateQuery(outfitId, uid);
-    void getOutfitByIdCate(queryAdvanced, res);
-  } else {
-    const queryBasic = `SELECT * FROM backend_schema.outfit WHERE oid = '${outfitId}' and uid = '${uid}'`;
-    void getOutfitById(queryBasic, res);
-  }
+	if (parse === 'categories') {
+		const queryAdvanced = outfitCateQuery(outfitId, uid);
+		void getOutfitByIdCate(queryAdvanced, res);
+	} else {
+		const queryBasic = `SELECT * FROM backend_schema.outfit WHERE oid = '${outfitId}' and uid = '${uid}'`;
+		void getOutfitById(queryBasic, res);
+	}
 });
 
 // Endpoint for retrieving all of the logged in user's outfits
 router.get('/', (req: Request, res: Response): void => {
-  const uid = req.user as string;
-  const { parse } = req.query;
+	const uid = req.user as string;
+	const { parse } = req.query;
 
-  if (parse === 'categories') {
-    const queryAdvanced = outfitCateQuery('', uid);
-    void getAllOutfitsCate(queryAdvanced, res);
-  } else {
-    const queryBasic = `SELECT * FROM backend_schema.outfit WHERE uid = '${uid}'`;
-    void getAllOutfits(queryBasic, res);
-  }
+	if (parse === 'categories') {
+		const queryAdvanced = outfitCateQuery('', uid);
+		void getAllOutfitsCate(queryAdvanced, res);
+	} else {
+		const queryBasic = `SELECT * FROM backend_schema.outfit WHERE uid = '${uid}'`;
+		void getAllOutfits(queryBasic, res);
+	}
 });
 
 export { router as default, router as privateOutfitRoute };
--- bottomLayer/src/routes/private/user.ts
+++ bottomLayer/src/routes/private/user.ts
@@ -1,9 +1,9 @@
 import express, { type Request, type Response } from 'express';
 import { pool } from '../../utils/sqlImport';
 import {
-  responseCallbackDelete,
-  responseCallbackUpdate,
-  responseCallbackGet
+	responseCallbackDelete,
+	responseCallbackUpdate,
+	responseCallbackGet,
 } from '../../utils/responseCallback';
 import { hash } from 'bcrypt';
 import { convertImage } from '../../s3/convert-image';
@@ -15,137 +15,135 @@ const router = express.Router();
 // Endpooint for getting the current user
 // Need this private get endpoint because the user may have private data that cannot be shared
 router.get('/', (req: Request, res: Response): void => {
-  const userId = req.user as string;
-  const getUser = async (): Promise<void> => {
-    try {
-      const user = await pool.query(
-        'SELECT * FROM backend_schema.user WHERE uid = $1',
-        [userId]
-      );
-      const result = user.rows[0];
-      const imgRef = result.pp_url;
-      result.pp_url = await downloadURLFromS3(imgRef);
+	const userId = req.user as string;
+	const getUser = async (): Promise<void> => {
+		try {
+			const user = await pool.query(
+				'SELECT * FROM backend_schema.user WHERE uid = $1',
+				[userId]
+			);
+			const result = user.rows[0];
+			const imgRef = result.pp_url;
+			result.pp_url = await downloadURLFromS3(imgRef);
 
-      responseCallbackGet(null, result, res, 'User');
-    } catch (error) {
-      responseCallbackGet(error, null, res);
-    }
-  };
-  void getUser();
+			responseCallbackGet(null, result, res, 'User');
+		} catch (error) {
+			responseCallbackGet(error, null, res);
+		}
+	};
+	void getUser();
 });
 
 // Endpoint for deleting a specific user
 router.delete('/', (req: Request, res: Response): void => {
-  const userId = req.user as string;
-  if (userId == null) return;
-  const deleteUser = async (): Promise<void> => {
-    try {
-      const deleteUser = await pool.query(
-        'DELETE FROM backend_schema.user WHERE uid = $1',
-        [userId]
-      );
-      responseCallbackDelete(null, userId, res, 'User', deleteUser.rowCount);
-    } catch (error) {
-      responseCallbackDelete(error, userId, res, 'User');
-    }
-  };
-  void deleteUser();
+	const userId = req.user as string;
+	if (userId == null) return;
+	const deleteUser = async (): Promise<void> => {
+		try {
+			const deleteUser = await pool.query(
+				'DELETE FROM backend_schema.user WHERE uid = $1',
+				[userId]
+			);
+			responseCallbackDelete(null, userId, res, 'User', deleteUser.rowCount);
+		} catch (error) {
+			responseCallbackDelete(error, userId, res, 'User');
+		}
+	};
+	void deleteUser();
 });
 
 // Endpoints for updating a specific user
 router.put(
-  '/',
-  upload.single('profile_picture'),
-  (req: Request, res: Response): void => {
-    const userId = req.user as string;
-    const fieldsToUpdate: User = req.body;
+	'/',
+	upload.single('profile_picture'),
+	(req: Request, res: Response): void => {
+		const userId = req.user as string;
+		const fieldsToUpdate: User = req.body;
 
-    const updateUser = async (): Promise<void> => {
-      let query = 'UPDATE backend_schema.user SET';
+		const updateUser = async (): Promise<void> => {
+			let query = 'UPDATE backend_schema.user SET';
 
-      let passAsync = null;
-      let ppAsync = null;
+			let passAsync = null;
+			let ppAsync = null;
 
-      try {
-        if (fieldsToUpdate.profile_picture !== undefined) {
-          ppAsync = convertImage(fieldsToUpdate.profile_picture, userId, false);
-        }
+			try {
+				if (fieldsToUpdate.profile_picture !== undefined) {
+					ppAsync = convertImage(fieldsToUpdate.profile_picture, userId, false);
+				}
 
-        if (fieldsToUpdate.password !== undefined) {
-          passAsync = hash(fieldsToUpdate.password, 10);
-        }
+				if (fieldsToUpdate.password !== undefined) {
+					passAsync = hash(fieldsToUpdate.password, 10);
+				}
 
-        if (fieldsToUpdate.first_name !== undefined) {
-          query += ` first_name = '${fieldsToUpdate.first_name}',`;
-        }
+				if (fieldsToUpdate.first_name !== undefined) {
+					query += ` first_name = '${fieldsToUpdate.first_name}',`;
+				}
 
-        if (fieldsToUpdate.last_name !== undefined) {
-          query += ` last_name = '${fieldsToUpdate.last_name}',`;
-        }
+				if (fieldsToUpdate.last_name !== undefined) {
+					query += ` last_name = '${fieldsToUpdate.last_name}',`;
+				}
 
-        if (fieldsToUpdate.email !== undefined) {
-          query += ` email = '${fieldsToUpdate.email.toLowerCase()}',`;
-        }
+				if (fieldsToUpdate.email !== undefined) {
+					query += ` email = '${fieldsToUpdate.email.toLowerCase()}',`;
+				}
 
-        if (fieldsToUpdate.username !== undefined) {
-          query += ` username = '${fieldsToUpdate.username}',`;
-        }
+				if (fieldsToUpdate.username !== undefined) {
+					query += ` username = '${fieldsToUpdate.username}',`;
+				}
 
-        if (fieldsToUpdate.password !== undefined) {
-          const hashedPass = await passAsync;
-          if (hashedPass === null) {
-            throw new Error('Impossible Null value occured');
-          }
-          query += ` password = '${hashedPass}',`;
-        }
+				if (fieldsToUpdate.password !== undefined) {
+					const hashedPass = await passAsync;
+					if (hashedPass === null) {
+						throw new Error('Impossible Null value occured');
+					}
+					query += ` password = '${hashedPass}',`;
+				}
 
-        if (fieldsToUpdate.private_option !== undefined) {
-          query += ` private_option = '${fieldsToUpdate.private_option.toString()}',`;
-        }
-        if (fieldsToUpdate.profile_picture !== undefined) {
-          const pp_url = await ppAsync;
-          if (pp_url === null) {
-            throw new Error('Impossible Null value occured');
-          }
-          query += ` pp_url = '${pp_url}',`;
-        }
+				if (fieldsToUpdate.private_option !== undefined) {
+					query += ` private_option = '${fieldsToUpdate.private_option.toString()}',`;
+				}
+				if (fieldsToUpdate.profile_picture !== undefined) {
+					const pp_url = await ppAsync;
+					if (pp_url === null) {
+						throw new Error('Impossible Null value occured');
+					}
+					query += ` pp_url = '${pp_url}',`;
+				}
 
-        if (fieldsToUpdate.followers !== undefined) {
-          query +=
-            ' followers = ' +
-            'ARRAY[' +
-            fieldsToUpdate.followers.join(', ') +
-            ']::UUID[]' +
-            ',';
-        }
+				if (fieldsToUpdate.followers !== undefined) {
+					const formattedUuidArray = fieldsToUpdate.followers.map(
+						(uuid) => `'${uuid}'::UUID`
+					);
 
-        if (fieldsToUpdate.following !== undefined) {
-          query +=
-            ' following = ' +
-            'ARRAY[' +
-            fieldsToUpdate.following.join(', ') +
-            ']::UUID[]' +
-            ',';
-        }
+					query += ` followers = ARRAY[${formattedUuidArray.join(', ')}],`;
+				}
 
-        if (query === 'UPDATE backend_schema.user SET') {
-          throw new Error('No Fields To Update');
-        }
+				if (fieldsToUpdate.following !== undefined) {
+					const formattedUuidArray = fieldsToUpdate.following.map(
+						(uuid) => `'${uuid}'::UUID`
+					);
 
-        query = query.slice(0, -1);
-        query += ` WHERE uid = '${userId}'`;
+					query += ` following = ARRAY[${formattedUuidArray.join(', ')}],`;
+				}
 
-        const update = await pool.query(query);
+				if (query === 'UPDATE backend_schema.user SET') {
+					throw new Error('No Fields To Update');
+				}
 
-        // Not sure if this returns updated user data also not sure if returning the new user data is needed
-        responseCallbackUpdate(null, userId, res, 'User', update.rowCount);
-      } catch (error) {
-        responseCallbackUpdate(error, userId, res, 'User');
-      }
-    };
+				query = query.slice(0, -1);
+				query += ` WHERE uid = '${userId}'`;
 
-    void updateUser();
-  }
+				const update = await pool.query(query);
+
+				// Not sure if this returns updated user data also not sure if returning the new user data is needed
+				responseCallbackUpdate(null, userId, res, 'User', update.rowCount);
+			} catch (error) {
+				responseCallbackUpdate(error, userId, res, 'User');
+			}
+		};
+
+		void updateUser();
+	}
 );
 
 export { router as default, router as privateUserRoute };
