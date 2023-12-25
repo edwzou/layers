1bf74f7ab186197e4bfcb48c1175774e2339855d
Doeun Kwon <dekwooon@gmail.com>
2023-12-25 11:59:23 -0800

    ignore Constants.platform.ios.model deprecated warning

--- topLayer/App.tsx
+++ topLayer/App.tsx
@@ -3,6 +3,7 @@ LogBox.ignoreLogs(['Require cycle:']);
 // ^ Ignores require cycle warnings. We decided to ignore these warnings for 2 reasons:
 // 1. Require cycles are technically not errors. It's just React Native telling us that this is an area of potential danger (which we should keep in mind)
 // 2. There's rarely a fix for require cycles, especially if it involves more than 2 components (which is our case)
+LogBox.ignoreLogs(['Constants.platform.ios.model has been deprecated']);
 
 import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
 import * as Device from 'expo-device';
