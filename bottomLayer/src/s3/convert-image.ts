cf5ebb6caca7b482c631122d0719dcfc6716ffcf
Edward Zou <edwzou@Edwards-MacBook-Air.local>
2023-12-28 14:40:26 -0700

    Removed sharp and added GIF checker

--- bottomLayer/src/s3/convert-image.ts
+++ bottomLayer/src/s3/convert-image.ts
@@ -1,21 +1,17 @@
 import { removeBackground } from './remove-background';
 import { uploadBufferToS3 } from './upload-buffer-to-s3';
-import sharp from 'sharp';
 
 async function isValidBase64String(base64: string): Promise<boolean> {
   if (typeof base64 !== 'string') {
     return false;
   }
+
+  if (base64.startsWith('R0lGOD')) {
+    throw new Error('This is a GIF image. Please input JPEG or PNG image.');
+  }
   
   const regex = /^([A-Za-z0-9+/]{4})*([A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{2}==)?$/; // REGEX checker
   return regex.test(base64);
-}
-
-async function convertToJpegBase64Buffer(base64: string): Promise<Buffer> {
-  const buffer = Buffer.from(base64, 'base64'); // Convert the base64 string to a buffer
-  return sharp(buffer) // Use sharp to convert the image to JPEG
-      .toFormat('jpeg')
-      .toBuffer();
 }
 
 async function convertImage(
@@ -28,7 +24,7 @@ async function convertImage(
   }
 
   try {
-    let imageBuffer = await convertToJpegBase64Buffer(base64);
+    let imageBuffer = Buffer.from(base64, 'base64');
 
     if (remove) {
       imageBuffer = await removeBackground(imageBuffer);
