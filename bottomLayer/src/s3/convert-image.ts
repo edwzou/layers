d027693bf398feb9ffda44b41391c72c45b88fd9
Edward Zou <edwzou@Edwards-MacBook-Air.local>
2023-12-28 16:35:38 -0700

    Added back sharp and fixed base64 string checker

--- bottomLayer/src/s3/convert-image.ts
+++ bottomLayer/src/s3/convert-image.ts
@@ -1,17 +1,18 @@
 import { removeBackground } from './remove-background';
 import { uploadBufferToS3 } from './upload-buffer-to-s3';
+import sharp from 'sharp';
 
 async function isValidBase64String(base64: string): Promise<boolean> {
-  if (typeof base64 !== 'string') {
-    return false;
-  }
+  return typeof base64 === 'string' &&
+    (base64.startsWith('/9j/') || base64.startsWith('iVBORw0KGgo') || base64.startsWith('R0lGOD')); // JPEG, PNG, GIF checker
+}
 
-  if (base64.startsWith('R0lGOD')) { // GIF checker
-    throw new Error('This is a GIF image. Please input JPEG or PNG image.');
-  }
-  
-  const regex = /^([A-Za-z0-9+/]{4})*([A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{2}==)?$/; // REGEX checker
-  return regex.test(base64);
+async function convertToJpegBase64Buffer(base64: string): Promise<Buffer> {
+  const buffer = Buffer.from(base64, 'base64'); // Convert the base64 string to a buffer
+  return sharp(buffer) // Use sharp to convert the image to JPEG
+      .rotate() // Rotate the image
+      .toFormat('jpeg')
+      .toBuffer();
 }
 
 async function convertImage(
@@ -24,7 +25,7 @@ async function convertImage(
   }
 
   try {
-    let imageBuffer = Buffer.from(base64, 'base64');
+    let imageBuffer = await convertToJpegBase64Buffer(base64);
 
     if (remove) {
       imageBuffer = await removeBackground(imageBuffer);
