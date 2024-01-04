32082c15e5079e54fe316e11227aa5e91dced28f
JoeDaBu <joegbu@gmail.com>
2024-01-04 11:31:14 -0700

    removed match-page context

--- topLayer/pages/Match/MatchPage.tsx
+++ topLayer/pages/Match/MatchPage.tsx
@@ -1,6 +1,6 @@
 import React, { useState, createContext, useContext } from 'react';
 import { StyleSheet } from 'react-native';
-import { StackNavigation, StepOverTypes } from '../../constants/Enums';
+import { StackNavigation } from '../../constants/Enums';
 import { Stack } from '../../utils/StackNavigation';
 import { headerButton } from '../../components/Modal/HeaderButton';
 import { NavigationContainer } from '@react-navigation/native';
@@ -18,107 +18,27 @@ import {
 } from '../../components/Toasts/Toasts';
 import { emptyClothing } from '../../constants/Clothing';
 
-export const MatchPageContext = createContext({
-	setMatch: (unused: any) => {},
-	dismissal: false,
-	isLoading: false,
-});
-
 const MatchPage: React.FC = () => {
-	const { setShouldRefreshMainPage } = useContext(MainPageContext);
-	const [dismissal, setDismissal] = useState(false);
-	const [isLoading, setIsLoading] = useState(false); // Add loading state
-
-	const [match, setMatch] = useState({
-		previewData: {
-			outerwear: { ...emptyClothing },
-			tops: { ...emptyClothing },
-			bottoms: { ...emptyClothing },
-			shoes: { ...emptyClothing },
-		},
-		matchName: '',
-	});
-
-	const onSubmit = (): void => {
-		const clothingItems = [
-			match.previewData.outerwear !== null &&
-			match.previewData.outerwear !== undefined
-				? match.previewData.outerwear.ciid
-				: null,
-			match.previewData.tops !== null && match.previewData.tops !== undefined
-				? match.previewData.tops.ciid
-				: null,
-			match.previewData.bottoms !== null &&
-			match.previewData.bottoms !== undefined
-				? match.previewData.bottoms.ciid
-				: null,
-			match.previewData.shoes !== null && match.previewData.shoes !== undefined
-				? match.previewData.shoes.ciid
-				: null,
-		].filter((item) => item !== null);
-		console.log('item: ', clothingItems);
-
-		const onSubmitInner = async (): Promise<void> => {
-			setIsLoading(true); // Start loading
-			try {
-				const response = await axios.post(`${baseUrl}/api/private/outfits`, {
-					title: match.matchName,
-					clothing_items: clothingItems,
-				});
-
-				if (response.status === 200) {
-					// alert(`You have created: ${JSON.stringify(response.data)}`);
-					setDismissal(true);
-					setShouldRefreshMainPage(true);
-					// navigationArray[0](); // Uncomment this to navigate to profile page
-					setDismissal(false);
-					showSuccessToast(toast.yourOutfitHasBeenCreated);
-				} else {
-					showErrorToast(toast.anErrorHasOccurredWhileCreatingOutfit);
-					// throw new Error('An error has occurred while submitting outfit');
-				}
-				setIsLoading(false); // Stop loading on success
-			} catch (error) {
-				setIsLoading(false); // Stop loading on error
-				axiosEndpointErrorHandler(error);
-			}
-		};
-		void onSubmitInner();
-	};
-
 	return (
-		<MatchPageContext.Provider value={{ setMatch, dismissal, isLoading }}>
-			<NavigationContainer independent={true}>
-				<Stack.Navigator>
-					<Stack.Screen
-						options={{
-							headerShown: false,
-						}}
-						name={StackNavigation.Match}
-						component={Match}
-					/>
-					<Stack.Screen
-						options={{
-							presentation: 'modal',
-							headerTitleStyle: GlobalStyles.typography.subtitle,
-							headerStyle: {
-								backgroundColor: GlobalStyles.colorPalette.background,
-							},
-							headerShadowVisible: false,
-							headerRight: () =>
-								headerButton({
-									type: StepOverTypes.done,
-									handlePress: () => {
-										onSubmit();
-									},
-								}),
-						}}
-						name={StackNavigation.OutfitPreview}
-						component={OutfitPreview}
-					/>
-				</Stack.Navigator>
-			</NavigationContainer>
-		</MatchPageContext.Provider>
+		<NavigationContainer independent={true}>
+			<Stack.Navigator>
+				<Stack.Screen
+					options={{
+						headerShown: false,
+					}}
+					name={StackNavigation.Match}
+					component={Match}
+				/>
+				<Stack.Screen
+					options={{
+						presentation: 'modal',
+						headerShown: false,
+					}}
+					name={StackNavigation.OutfitPreview}
+					component={OutfitPreview}
+				/>
+			</Stack.Navigator>
+		</NavigationContainer>
 	);
 };
 
--- topLayer/pages/OutfitPreview/OutfitPreview.tsx
+++ topLayer/pages/OutfitPreview/OutfitPreview.tsx
@@ -6,7 +6,8 @@ import React, {
 	type ReactElement,
 } from 'react';
 import { FlatList } from 'react-native-gesture-handler';
-
+import { baseUrl } from '../../utils/apiUtils';
+import axios from 'axios';
 import ItemCell from '../../components/Cell/ItemCell';
 import GlobalStyles from '../../constants/GlobalStyles';
 import StackedTextbox from '../../components/Textbox/StackedTextbox';
@@ -14,28 +15,45 @@ import StackedTextbox from '../../components/Textbox/StackedTextbox';
 import { ITEM_SIZE } from '../../utils/GapCalc';
 import { screenHeight } from '../../utils/modalMaxShow';
 import { type UserClothing } from '../../types/Clothing';
-import { match } from '../../constants/GlobalStrings';
-
 import { MatchPageContext } from '../../pages/Match/MatchPage';
-
+import { axiosEndpointErrorHandler } from '../../utils/ErrorHandlers';
+import { toast } from '../../constants/GlobalStrings';
+import {
+	showErrorToast,
+	showSuccessToast,
+} from '../../components/Toasts/Toasts';
 import { useNavigation } from '@react-navigation/native';
 import { type NativeStackNavigationProp } from '@react-navigation/native-stack';
-import { type StackTypes } from 'utils/StackNavigation';
+import { type StackTypes } from '../../utils/StackNavigation';
 import { Loading } from '../../components/Loading/Loading';
+import { StackNavigation, StepOverTypes } from '../../constants/Enums';
+import Header from '../../components/Header/Header';
+import { emptyClothing } from '../../constants/Clothing';
+import { MainPageContext } from '../../pages/Main/MainPage';
 
 const OutfitPreview = ({ route }: any): ReactElement => {
 	const navigation = useNavigation<NativeStackNavigationProp<StackTypes>>();
 
 	const { matchItems } = route.params;
 
-	const { setMatch, dismissal, isLoading } = useContext(MatchPageContext);
+	const { setShouldRefreshMainPage } = useContext(MainPageContext);
 
 	const [text, setText] = useState('');
+	const [isLoading, setIsLoading] = useState(false); // Add loading state
 	const [rawData, setRawData] = useState<UserClothing[]>([]);
 	const [data, setData] = useState<UserClothing[]>([]);
 	const onInputChange = (text: string): void => {
 		setText(text);
 	};
+	const [match, setMatch] = useState({
+		previewData: {
+			outerwear: { ...emptyClothing },
+			tops: { ...emptyClothing },
+			bottoms: { ...emptyClothing },
+			shoes: { ...emptyClothing },
+		},
+		matchName: '',
+	});
 
 	useEffect(() => {
 		setRawData([
@@ -60,45 +78,99 @@ const OutfitPreview = ({ route }: any): ReactElement => {
 		setData(rawData.filter(Boolean));
 	}, [rawData]);
 
-	useEffect(() => {
-		if (dismissal) {
-			navigation.goBack();
-		}
-	}, [dismissal]);
+	const onSubmit = (): void => {
+		const clothingItems = [
+			match.previewData.outerwear !== null &&
+			match.previewData.outerwear !== undefined
+				? match.previewData.outerwear.ciid
+				: null,
+			match.previewData.tops !== null && match.previewData.tops !== undefined
+				? match.previewData.tops.ciid
+				: null,
+			match.previewData.bottoms !== null &&
+			match.previewData.bottoms !== undefined
+				? match.previewData.bottoms.ciid
+				: null,
+			match.previewData.shoes !== null && match.previewData.shoes !== undefined
+				? match.previewData.shoes.ciid
+				: null,
+		].filter((item) => item !== null);
+		console.log('item: ', clothingItems);
 
+		const onSubmitInner = async (): Promise<void> => {
+			setIsLoading(true); // Start loading
+			try {
+				const response = await axios.post(`${baseUrl}/api/private/outfits`, {
+					title: match.matchName,
+					clothing_items: clothingItems,
+				});
+
+				if (response.status === 200) {
+					// alert(`You have created: ${JSON.stringify(response.data)}`);
+
+					navigation.goBack();
+					setShouldRefreshMainPage(true);
+					// navigationArray[0](); // Uncomment this to navigate to profile page
+					showSuccessToast(toast.yourOutfitHasBeenCreated);
+				} else {
+					showErrorToast(toast.anErrorHasOccurredWhileCreatingOutfit);
+					// throw new Error('An error has occurred while submitting outfit');
+				}
+				setIsLoading(false); // Stop loading on success
+			} catch (error) {
+				setIsLoading(false); // Stop loading on error
+				axiosEndpointErrorHandler(error);
+			}
+		};
+		void onSubmitInner();
+	};
 	return (
 		<View style={styles.container}>
-			<StackedTextbox
-				label={match.matchName}
-				onFieldChange={onInputChange}
-				value={text}
+			<Header
+				text={StackNavigation.OutfitPreview}
+				rightButton={true}
+				rightStepOverType={StepOverTypes.done}
+				rightButtonAction={() => {
+					onSubmit();
+				}}
 			/>
-			<FlatList
-				data={data}
-				renderItem={({ item }) => (
-					<View style={{ width: ITEM_SIZE(2) }}>
-						<ItemCell
-							imageUrl={item.image_url}
-							disablePress={false}
-							key={item.ciid}
-						/>
-					</View>
-				)}
-				numColumns={2}
-				contentContainerStyle={{ gap: GlobalStyles.layout.gap }}
-				columnWrapperStyle={{ gap: GlobalStyles.layout.gap }}
-				style={{ height: screenHeight - 350 }}
-			/>
+			<View style={styles.containerInner}>
+				<StackedTextbox
+					label={match.matchName}
+					onFieldChange={onInputChange}
+					value={text}
+				/>
+				<FlatList
+					data={data}
+					renderItem={({ item }) => (
+						<View style={{ width: ITEM_SIZE(2) }}>
+							<ItemCell
+								imageUrl={item.image_url}
+								disablePress={false}
+								key={item.ciid}
+							/>
+						</View>
+					)}
+					numColumns={2}
+					contentContainerStyle={{ gap: GlobalStyles.layout.gap }}
+					columnWrapperStyle={{ gap: GlobalStyles.layout.gap }}
+					style={{ height: screenHeight - 350 }}
+				/>
 
-			{isLoading && <Loading />}
+				{isLoading && <Loading />}
+			</View>
 		</View>
 	);
 };
 
 const styles = StyleSheet.create({
-	container: {
+	containerInner: {
 		marginHorizontal: GlobalStyles.layout.xGap,
 		gap: GlobalStyles.layout.gap,
+	},
+	container: {
+		gap: GlobalStyles.layout.gap,
+		paddingTop: 20,
 	},
 });
 
