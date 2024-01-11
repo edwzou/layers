dbae9f22c9914307fbd253235576f49fb88672a1
JoeDaBu <joegbu@gmail.com>
2024-01-10 18:18:35 -0800

    fixed stack navigation typing

--- topLayer/pages/ItemView/ItemViewPage.tsx
+++ topLayer/pages/ItemView/ItemViewPage.tsx
@@ -5,17 +5,15 @@ import {
 	NavigationContainer,
 	type RouteProp,
 	useNavigation,
+	useRoute,
 } from '@react-navigation/native';
 import { StackNavigation } from '../../constants/Enums';
 import { type NativeStackNavigationProp } from '@react-navigation/native-stack';
 import React, { type ReactElement } from 'react';
 import { type RouteTypes } from 'types/Routes';
 
-const ItemViewPage = ({
-	route,
-}: {
-	route: RouteProp<RouteTypes, 'ItemViewPage'>;
-}): ReactElement => {
+const ItemViewPage = (): ReactElement => {
+	const route = useRoute<RouteProp<RouteTypes, 'ItemViewPage'>>();
 	const { item, editable } = route.params;
 
 	const navigation = useNavigation<NativeStackNavigationProp<StackTypes>>();
