0eb4b146c208b0aa7c7a697ae252d2fdea7b07f2
JoeDaBu <joegbu@gmail.com>
2024-01-14 12:42:40 -0800

    fixed type imports"

--- topLayer/functions/Profile/Profile.tsx
+++ topLayer/functions/Profile/Profile.tsx
@@ -1,6 +1,6 @@
-import { type UserAllItems } from '@/types/AllItems';
-import { type UserClothing } from '@/types/Clothing';
-import { type UserOutfit } from '@/types/Outfit';
+import { type UserAllItems } from '../../types/AllItems';
+import { type UserClothing } from '../../types/Clothing';
+import { type UserOutfit } from '../../types/Outfit';
 import { CategoryToIndex, StackNavigation } from '../../constants/Enums';
 import { type Dispatch, type RefObject, type SetStateAction } from 'react';
 import { type FlatList } from 'react-native';
